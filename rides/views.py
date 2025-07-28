6478d9ab06223bdff469aab9ffcd62ea6afa27c4
PRASEED S <140156199+Praseed35@users.noreply.github.com>
2025-07-28 16:56:49 +0530

    fixing signup bug in views.py

--- rides/views.py
+++ rides/views.py
@@ -183,7 +183,7 @@ def user_signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dashboard')
+            return redirect('home')
     else:
         form = SignUpForm()
     
